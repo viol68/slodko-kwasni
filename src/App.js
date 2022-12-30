de1f3e123e05bc12fc8b48e9c6b7aad9d6d565e5
Kanonashi <100380552+Kanonashi@users.noreply.github.com>
2022-12-30 14:39:02 +0100

    add user3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 const user1 = "4lip"
 const user2 = "Hudziak"
+const user3 = "Kanonashi"
+
 
 function App()
 {
@@ -8,6 +10,7 @@ function App()
     <div className="App">
      {user1}
      {user2}
+     {user3}
     </div>
   );
 }
