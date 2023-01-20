dc3ef38d8ae32cc4f81826c794e30a20c71e4338
Gizmo <michaal.duszynski@gmail.com>
2023-01-20 12:58:11 +0100

    string

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
         <div>{user.name + user.codeGoal + "Kanonashi"}</div>
       ))}
       {users.map((user) => (
-        <div>{user.name.substring() + user.codeGoal.toLowerCase() + "Gizmo"}</div>
+        <div>{user.name.substring(3) + user.codeGoal.toLowerCase() + "Gizmo"}</div>
       ))}
       {users.map((user) => (
         <div>
@@ -60,7 +60,7 @@ function App() {
         <div>{user.name.charAt(3) + user.codeGoal.length + "Hudziak"}</div>
       ))}
       {users.map((user) => (
-        <div>{user.name + user.codeGoal + "Dorka"}</div>
+        <div>{user.name.replace() + user.codeGoal.endsWith("a") + "Dorka"}</div>
       ))}
       {users.map((user) => (
         <div>{user.name.concat(" ", " Koksi") + user.codeGoal.indexOf("portfolio") + "Roxi"}</div>
