eab788595f388616b3ee0bc5efc4d52847db4637
4lipp <100539670+4lipp@users.noreply.github.com>
2023-01-27 15:03:23 +0100

    description

--- src/components/atoms/Description.jsx
+++ src/components/atoms/Description.jsx
@@ -1,5 +1,8 @@
+import styled from "styled-components";
+
 // 4lip
 export const Description = (props) => {
   console.log("elo1");
-  return <div>Filip</div>;
+  return <StyledWraper>Filip </StyledWraper>;
 };
+const StyledWraper = styled("p")(()=>({backgroundColor: "red"}))
