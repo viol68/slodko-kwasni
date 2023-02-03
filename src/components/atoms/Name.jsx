89af9d4c62ffdc65844713e102afa46762477a83
JoanSido <101212550+JoanSido@users.noreply.github.com>
2023-02-03 14:55:59 +0100

    kółki i imie

--- src/components/atoms/Name.jsx
+++ src/components/atoms/Name.jsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+
 // Grumcio
 export const Name = (props) => {
   console.log(props.userData);
-  return <div>{props.userData.name}</div>;
+  return (
+    <StyledWraper>
+      <StyledDot />
+      {props.userData.name}
+    </StyledWraper>
+  );
 };
+const StyledWraper = styled("p")(() => ({
+  fontSize: "20px",
+  display: "flex",
+  gap: "15px",
+}));
+const StyledDot = styled("div")(() => ({
+  backgroundColor: "rgb(255,89,37)",
+  width: "30px",
+  height: "30px",
+  borderRadius: "50%",
+}));
